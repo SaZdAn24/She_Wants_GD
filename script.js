2063f7f8bd12eb95b272da4c74263030cfd24922
DaniK <admin@MacBook-Pro-Deni.local>
2024-10-26 13:10:05 +0200

    fix button modal window

--- script.js
+++ script.js
@@ -49,3 +49,16 @@ document.getElementById('search-button').addEventListener('click', function() {
         alert('Секция не найдена. Попробуйте другой запрос.');
     }
 });
+// Закрытие модального окна при нажатии на крестик
+document.querySelector(".close-button").addEventListener("click", function() {
+    const modal = document.getElementById("confirmation-modal");
+    modal.style.display = "none"; // Скрыть модальное окно
+});
+
+// Закрытие модального окна при клике вне его области
+window.addEventListener("click", function(event) {
+    const modal = document.getElementById("confirmation-modal");
+    if (event.target === modal) {
+        modal.style.display = "none"; // Скрыть модальное окно
+    }
+});
